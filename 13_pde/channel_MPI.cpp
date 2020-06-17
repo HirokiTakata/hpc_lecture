ea482f7b846b9e226c2c8eeb1c67db249f2b1f2d
Hiroki Takata <sakuradadadash555@gmail.com>
2020-06-17 15:14:46 +0900

    channel_MPI add

--- 13_pde/channel_MPI.cpp
+++ 13_pde/channel_MPI.cpp
@@ -4,10 +4,9 @@
 #include<iterator>
 #include<fstream>
 #include<cmath>
-//#include"matplotlib-cpp/matplotlibcpp.h"
+#include"mpi.h"
 
 using namespace std;
-//namespace plt = matplotlibcpp;
 
 vector<vector<double> > build_up_b(double rho,double dt,double dx,double dy,vector<vector<double> > u,vector<vector<double> > v){
 	vector< vector<double> > b(u.size(),vector<double>(u.at(0).size(),0));
@@ -18,12 +17,10 @@ vector<vector<double> > build_up_b(double rho,double dt,double dx,double dy,vect
 	}
 	int n = b.at(0).size();
 	for(int i=1;i<b.size()-1;i++){
-		//for(int j=1;j<b.at(0).size()-1;j++){
 		b[i][n-1] = (double)(rho*(1/dt*((u[i][0]-u[i][n-2]) / (2*dx) + (v[i+1][n-1] - v[i-1][n-1]) / (2*dy)) - pow(((u[i][0]-u[i][n-2]) / (2*dx)),2) - 2*((u[i+1][n-1] - u[i-1][n-1]) / (2*dy) * (v[i][0]-v[i][n-2]) / (2*dx)) - pow(((v[i+1][n-1]-v[i-1][n-1]) / (2*dy)),2)));
 	}
 
 	for(int i=1;i<b.size()-1;i++){
-		//for(int j=1;j<b.at(0).size()-1;j++){
 		b[i][0] = (double)(rho*(1/dt*((u[i][1]-u[i][n-1]) / (2*dx) + (v[i+1][0] - v[i-1][0]) / (2*dy)) - pow(((u[i][1]-u[i][n-1]) / (2*dx)),2) - 2*((u[i+1][0]-u[i-1][0]) / (2*dy) * (v[i][1]-v[i][n-1]) / (2*dx)) - pow(((v[i+1][0]-v[i-1][0]) / (2*dy)),2)));
 	}
 	
@@ -67,6 +64,10 @@ vector<vector<double> > pressure_poisson_periodic(vector<vector<double> > p,vect
 	return p;
 }
 int main(int argc,char** argv){
+	MPI_Init(&argc,&argv);
+	int size,rank;
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	int nx = 41;
 	int ny = 41;
 	int nt = 10;
@@ -100,15 +101,23 @@ int main(int argc,char** argv){
 	double F = 1;
 	double dt = 0.01;
 	vector<vector<double> > u(ny, vector<double>(nx, 0));
+	//double u[ny][nx];
 	vector<vector<double> > un(ny, vector<double>(nx, 0));
 	vector<vector<double> > v(ny, vector<double>(nx, 0));
+	//double v[ny][nx];
 	vector<vector<double> > vn(ny, vector<double>(nx, 0));
 	vector<vector<double> > p(ny, vector<double>(nx, 0));
 	vector<vector<double> > pn(ny, vector<double>(nx, 0));
 	vector<vector<double> > b(ny, vector<double>(nx, 0));
 	double udiff = 1;	
 	int stepcount = 0;
-	while(udiff > 0.001){
+	int num = (ny-2)/(size-1);
+	int start = (rank-1)*num+1;
+	int end = start+num;
+	double buffer[num*2][nx];
+	double ucopy[ny][nx];
+	double vcopy[ny][nx];
+	while(udiff > 0.001){ 	
 		for(int i=0;i<un.size();i++){
 			for(int j=0;j<un.at(0).size();j++){
 				un[i][j] = u[i][j];
@@ -118,64 +127,106 @@ int main(int argc,char** argv){
 
 		b = build_up_b(rho,dt,dx,dy,u,v);
 		p = pressure_poisson_periodic(p,b,dx,dy,nit);
-		
-			
 		int n = u.at(0).size();
-		for(int i=1;i<u.size()-1;i++){
-			for(int j=1;j<u.at(0).size()-1;j++){
-				u[i][j] = (un[i][j]-un[i][j]*dt/dx*(un[i][j]-un[i][j-1])-vn[i][j]*dt/dy*(un[i][j]-un[i-1][j])-dt/(2*rho*dx)*(p[i][j+1]-p[i][j-1])+nu*(dt/pow(dx,2)*(un[i][j+1]-2*un[i][j]+un[i][j-1])+dt/pow(dy,2)*(un[i+1][j]-2*un[i][j]+un[i-1][j]))+F*dt);
+		
+		//ホストとクライアントに分け、分散処理
+		if(rank!=0){
+			int l=0;	
+			for(int i=start;i<end;i++){
+				for(int j=1;j<u.at(0).size()-1;j++){
+					buffer[l][j] = (un[i][j]-un[i][j]*dt/dx*(un[i][j]-un[i][j-1])-vn[i][j]*dt/dy*(un[i][j]-un[i-1][j])-dt/(2*rho*dx)*(p[i][j+1]-p[i][j-1])+nu*(dt/pow(dx,2)*(un[i][j+1]-2*un[i][j]+un[i][j-1])+dt/pow(dy,2)*(un[i+1][j]-2*un[i][j]+un[i-1][j]))+F*dt);
+				}
+				l++;	
 			}
-		}
 	
-	
-		n = v.at(0).size();
-		for(int i=1;i<v.size()-1;i++){
-			for(int j=1;j<v.at(0).size()-1;j++){
-				v[i][j] = (vn[i][j]-un[i][j]*dt/dx*(vn[i][j]-vn[i][j-1])-vn[i][j]*dt/dy*(vn[i][j]-vn[i-1][j])-dt/(2*rho*dy)*(p[i+1][j]-p[i-1][j])+nu*(dt/pow(dx,2)*(vn[i][j+1]-2*vn[i][j]+vn[i][j-1])+dt/pow(dy,2)*(vn[i+1][j]-2*vn[i][j]+vn[i-1][j])));
+			n = v.at(0).size();
+			for(int i=start;i<end;i++){
+				for(int j=1;j<v.at(0).size()-1;j++){
+					buffer[l][j] = (vn[i][j]-un[i][j]*dt/dx*(vn[i][j]-vn[i][j-1])-vn[i][j]*dt/dy*(vn[i][j]-vn[i-1][j])-dt/(2*rho*dy)*(p[i+1][j]-p[i-1][j])+nu*(dt/pow(dx,2)*(vn[i][j+1]-2*vn[i][j]+vn[i][j-1])+dt/pow(dy,2)*(vn[i+1][j]-2*vn[i][j]+vn[i-1][j])));
+				}
+				l++;
 			}
-		}
-		
-		
-		n = u.at(0).size();
-		for(int i=1;i<u.size()-1;i++){
+    			MPI_Send(buffer, num*2*nx, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    			MPI_Recv(ucopy, ny*nx, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    			MPI_Recv(vcopy, ny*nx, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&udiff,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			for(int i=0;i<ny;i++){
+				for(int j=0;j<nx;j++){
+					u[i][j] = ucopy[i][j];
+					v[i][j] = vcopy[i][j];
+				}
+			}
 
-			u[i][n-1] = (un[i][n-1]-un[i][n-1]*dt/dx*(un[i][n-1]-un[i][n-2])-vn[i][n-1]*dt/dy*(un[i][n-1]-un[i-1][n-1])-dt/(2*rho*dx)*(p[i][0]-p[i][n-2])+nu*(dt/pow(dx,2)*(un[i][0]-2*un[i][n-1]+un[i][n-2])+dt/pow(dy,2)*(un[i+1][n-1]-2*un[i][n-1]+un[i-1][n-1]))+F*dt);
-			u[i][0] = (un[i][0]-un[i][0]*dt/dx*(un[i][0]-un[i][n-1])-vn[i][0]*dt/dy*(un[i][0]-un[i-1][0])-dt/(2*rho*dx)*(p[i][1]-p[i][n-1])+nu*(dt/pow(dx,2)*(un[i][1]-2*un[i][0]+un[i][n-1])+dt/pow(dy,2)*(un[i+1][0]-2*un[i][0]+un[i-1][0]))+F*dt);
+		}else{
+			for(int i=1;i<u.size()-1;i++){
 
-		}			
+				u[i][n-1] = (un[i][n-1]-un[i][n-1]*dt/dx*(un[i][n-1]-un[i][n-2])-vn[i][n-1]*dt/dy*(un[i][n-1]-un[i-1][n-1])-dt/(2*rho*dx)*(p[i][0]-p[i][n-2])+nu*(dt/pow(dx,2)*(un[i][0]-2*un[i][n-1]+un[i][n-2])+dt/pow(dy,2)*(un[i+1][n-1]-2*un[i][n-1]+un[i-1][n-1]))+F*dt);
+				u[i][0] = (un[i][0]-un[i][0]*dt/dx*(un[i][0]-un[i][n-1])-vn[i][0]*dt/dy*(un[i][0]-un[i-1][0])-dt/(2*rho*dx)*(p[i][1]-p[i][n-1])+nu*(dt/pow(dx,2)*(un[i][1]-2*un[i][0]+un[i][n-1])+dt/pow(dy,2)*(un[i+1][0]-2*un[i][0]+un[i-1][0]))+F*dt);
+
+			}			
 
 
-		n = v.at(0).size();
-		for(int i=1;i<v.size()-1;i++){
+			n = v.at(0).size();
+			for(int i=1;i<v.size()-1;i++){
 
-			v[i][n-1] = (vn[i][n-1]-un[i][n-1]*dt/dx*(vn[i][n-1]-vn[i][n-2])-vn[i][n-1]*dt/dy*(vn[i][n-1]-vn[i-1][n-1])-dt/(2*rho*dy)*(p[i+1][n-1]-p[i-1][n-1])+nu*(dt/pow(dx,2)*(vn[i][0]-2*vn[i][n-1]+vn[i][n-2])+dt/pow(dy,2)*(vn[i+1][n-1]-2*vn[i][n-1]+vn[i-1][n-1])));
-			v[i][0] = (vn[i][0]-un[i][0]*dt/dx*(vn[i][0]-vn[i][n-1])-vn[i][0]*dt/dy*(vn[i][0]-vn[i-1][0])-dt/(2*rho*dy)*(p[i+1][0]-p[i-1][0])+nu*(dt/pow(dx,2)*(vn[i][1]-2*vn[i][0]+vn[i][n-1])+dt/pow(dy,2)*(vn[i+1][0]-2*vn[i][0]+vn[i-1][0])));
+				v[i][n-1] = (vn[i][n-1]-un[i][n-1]*dt/dx*(vn[i][n-1]-vn[i][n-2])-vn[i][n-1]*dt/dy*(vn[i][n-1]-vn[i-1][n-1])-dt/(2*rho*dy)*(p[i+1][n-1]-p[i-1][n-1])+nu*(dt/pow(dx,2)*(vn[i][0]-2*vn[i][n-1]+vn[i][n-2])+dt/pow(dy,2)*(vn[i+1][n-1]-2*vn[i][n-1]+vn[i-1][n-1])));
+				v[i][0] = (vn[i][0]-un[i][0]*dt/dx*(vn[i][0]-vn[i][n-1])-vn[i][0]*dt/dy*(vn[i][0]-vn[i-1][0])-dt/(2*rho*dy)*(p[i+1][0]-p[i-1][0])+nu*(dt/pow(dx,2)*(vn[i][1]-2*vn[i][0]+vn[i][n-1])+dt/pow(dy,2)*(vn[i+1][0]-2*vn[i][0]+vn[i-1][0])));
 
-		}			
-		n = u.at(0).size();
-		for(int j=0;j<u.at(0).size();j++){
-			u[0][j] = 0;
-			u[n-1][j] = 0;
-		}
-		n = v.at(0).size();
-		for(int j=0;j<v.at(0).size();j++){
-			v[0][j] = 0;
-			v[n-1][j] = 0;
-		}
-		
-		double usum = 0;
-		double unsum = 0;
-		for(int i=0;i<u.size();i++){
+			}				
+			n = u.at(0).size();
 			for(int j=0;j<u.at(0).size();j++){
-				usum += u[i][j];
-				unsum += un[i][j];
+				u[0][j] = 0;
+				u[n-1][j] = 0;
+			}
+			n = v.at(0).size();
+			for(int j=0;j<v.at(0).size();j++){
+				v[0][j] = 0;
+				v[n-1][j] = 0;
+			}
+		
+	
+		//分散処理させた結果をホストで統合
+		
+			for(int irank=1;irank<size;irank++){
+				MPI_Recv(buffer,num*2*nx,MPI_DOUBLE,irank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+				for(int i=0;i<num;i++){
+					for(int j=1;j<nx-1;j++){
+						u[(irank-1)*num+1+i][j] = buffer[i][j];
+					}
+				}
+				for(int i=num;i<num*2;i++){
+					for(int j=1;j<nx-1;j++){
+						v[(irank-1)*num+1+i-num][j] = buffer[i][j];
+					}
+				}
+			}
+			
+			double usum = 0;
+			double unsum = 0;
+			for(int i=0;i<u.size();i++){
+				for(int j=0;j<u.at(0).size();j++){
+					usum += u[i][j];
+					unsum += un[i][j];
+				}
+			}
+		
+			udiff = (usum - unsum)/usum;
+			stepcount+=1;
+			
+			for(int i=0;i<ny;i++){
+				for(int j=0;j<nx;j++){
+					ucopy[i][j] = u[i][j];
+					vcopy[i][j] = v[i][j];
+				}
+			}
+			for(int irank=1;irank<size;irank++){
+				MPI_Send(ucopy,ny*nx,MPI_DOUBLE,irank,0,MPI_COMM_WORLD);
+				MPI_Send(vcopy,ny*nx,MPI_DOUBLE,irank,0,MPI_COMM_WORLD);
+				MPI_Send(&udiff,1,MPI_DOUBLE,irank,0,MPI_COMM_WORLD);
 			}
 		}
-
-		udiff = (usum - unsum)/usum;
-		stepcount+=1;
 	}	
-	cout<<stepcount<<endl;
+	if(rank==0){cout<<stepcount<<endl;}
 	
 	string filename = "output.txt";
 	ofstream writing_file;
@@ -190,6 +241,6 @@ int main(int argc,char** argv){
 		}
 	}
 		
-	
+	MPI_Finalize();
 
 }
